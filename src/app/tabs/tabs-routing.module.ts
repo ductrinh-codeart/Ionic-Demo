081970d877be92a70b7ba5b0ce1fd81bd15b677a
Minh Đức <82555904+ductrinh-codeart@users.noreply.github.com>
2021-06-21 20:58:08 +0700

    fix tab2 Cam can't capture img

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -27,26 +27,6 @@ const routes: Routes = [
         path: 'tab5',
         loadChildren: () => import('../tab5/tab5.module').then( m => m.Tab5PageModule)
       },
-      {
-        path: '/tabs/tab1',
-        loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
-      },
-      {
-        path: '/tabs/tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
-      },
-      {
-        path: '/tabs/tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
-      },
-      {
-        path: '/tabs/tab4',
-        loadChildren: () => import('../tab4/tab4.module').then(m => m.Tab4PageModule)
-      },
-      {
-        path: '/tabs/tab5',
-        loadChildren: () => import('../tab5/tab5.module').then(m => m.Tab5PageModule)
-      },
     ]
   },
   {
